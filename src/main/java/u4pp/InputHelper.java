7b26d087300b8f7a2fd59deb078abd5a852a37cd
Rebecca Kim <rebeccakim@MacBook-Pro-7.attlocal.net>
2022-12-03 20:50:48 -0800

    re

--- src/main/java/u4pp/InputHelper.java
+++ src/main/java/u4pp/InputHelper.java
@@ -15,30 +15,34 @@
 package u4pp;
 import java.util.Scanner;
 
-// public class InputHelper {
-//     Scanner scanner;
+public class InputHelper {
+    Scanner scanner;
 
-//     public void InputHelper(Scanner scanner) {
-//         this.scanner = scanner;
-//     }
+    public InputHelper(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
-//     public boolean getYesNoInput(String prompt) {
-//         System.out.println(prompt);
-//         String userInput = this.scanner.next();
-//         char firstChar = userInput.charAt(0);
-//         String String = "" + firstChar;
-//         Boolean valid = String.equals("Y");  String.equals("y");  String.equals("n");
-//                  String.equals("N");
-//         return valid;
-//     }
+    public boolean getYesNoInput(String prompt) {
+        System.out.println(prompt);
+        String userInput = this.scanner.next();
+        char firstChar = userInput.charAt(0);
+        String String = "" + firstChar;
+        Boolean valid = String.equals("Y");  String.equals("y");  String.equals("n");
+                 String.equals("N");
+        return valid;
+    }
 
-//     public int getIntegerInput(String prompt, int min, int max) {
-//         int i = min - 1;
-//         while (i <= min && i >= max) {
-//             System.out.println(prompt);
-//             String userInput = this.scanner.next();
-//             i = Integer.parseInt(userInput);
-//         }
-//         return i;
-//     }
-// }
+    public int getIntegerInput(String prompt, int min, int max) {
+        int number;
+        do {
+            System.out.println(prompt);
+            while (!scanner.hasNextInt()){
+                System.out.println("That's not a valid number!");
+                scanner.next();
+            }
+            number = scanner.nextInt();
+        } while (number >= min && number <= max);
+        return number;
+        
+    }
+}
--- src/main/java/u4pp/LeapYear.java
+++ src/main/java/u4pp/LeapYear.java
@@ -5,12 +5,21 @@ public class LeapYear {
     
     public static boolean isLeapYear(int year){
         boolean isLeapYear = true;
-        if (year > 1582 && year % 400 == 0 || year % 4 == 0){
+        if (year > 1582 && year % 4 == 0){
+            if (year % 400 == 0){
             return isLeapYear;
+            }
+
+            else if (year % 100 == 0){
+                return false;
+            }
+            
+            else {
+                return isLeapYear;
+            }
         }
-        
         else {
-            return isLeapYear = false;
+            return false;
         }
     }
     
