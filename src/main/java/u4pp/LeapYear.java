153e4ea91559740af340a7d42d36e46eefb83b52
rebelcan <113130590+rebelcan@users.noreply.github.com>
2022-12-15 20:18:50 -0800

    Update LeapYear.java

--- src/main/java/u4pp/LeapYear.java
+++ src/main/java/u4pp/LeapYear.java
@@ -5,7 +5,9 @@ public class LeapYear {
     
     public static boolean isLeapYear(int year){
         boolean isLeapYear = true;
+        //setting the conditions
         if (year > 1582 && year % 4 == 0){
+            //narrowing conditions down from %4 to %400
             if (year % 400 == 0){
             return isLeapYear;
             }
